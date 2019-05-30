51e8d33c97c9782f2903f2bc2d29e56deaff9d97
jonjozwiak <jozwiaj@amazon.com>
2019-05-30 18:02:01 +0000

    Updating index.php

--- src/index.php
+++ src/index.php
@@ -2,6 +2,7 @@
 
 echo "Hello, World from Kubernetes! <br>";
 echo "Hello Jenkins X! <br>";
+echo "Test take 2 - committed! <br>";
 echo '<img border="0" src="https://jenkins-x.io/images/logo.png" width="300" height="300" />';
 
 ?>
